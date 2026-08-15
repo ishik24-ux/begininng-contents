7af8a961ba22eabc8cc4123e817363309e17c3f5
ishik24-ux <ishikawanaru24@gmail.com>
2026-08-15 10:50:04 +0900

    Add api/logout.js

---
+++ api/logout.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end();
+  res.setHeader('Set-Cookie', 'begininng_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
+  return res.status(204).end();
+}
+
